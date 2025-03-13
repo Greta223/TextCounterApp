074bffbd20b07553e0ba57f48c115bb938c3b2b8
Greta Gabrėnaitė <greta.gabrenaite@gmail.com>
2025-03-13 14:05:11 +0200

    Refactor UI layout and add button functionality
    This commit refactors the main activity layout and introduces a button with interactive behavior.
    
    Key changes:
    
    -   **UI Layout**:
        - Changed the text in `TextView` to display "Hello, Centered Text!".
        - Added a `Button` with the text "Submit".
        - Positioned the `Button` below the `TextView`.
    -   **Button Functionality**:
        - Added a click listener to the `Button`.
        - Updated the `TextView` text to "Button Clicked!" when the `Button` is clicked.
    - **EdgeToEdge listener**:
        - Changed the listener from the view with ID `main` to `android.R.id.content` to cover all the screen content.

--- app/src/main/java/com/example/fintechapp/MainActivity.java
+++ app/src/main/java/com/example/fintechapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.fintechapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,7 +17,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        TextView myTextView = findViewById(R.id.myTextView);
+        Button myButton = findViewById(R.id.myButton);
+        myButton.setOnClickListener(view -> {
+            myTextView.setText("Button Clicked!");
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
